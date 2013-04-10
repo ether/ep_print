c356b6445c8b1fbdc406c2ee2b4fe8788d030308
John McLear <john@mclear.co.uk>
2013-04-10 18:09:55 +0100

    working

--- static/js/print.js
+++ static/js/print.js
@@ -1,35 +1,42 @@
 exports.postAceInit = function(name, context, cb){
+
   (function() { // From http://stackoverflow.com/questions/1234008/detecting-browser-print-event/11060206#11060206 -- License unknown
+
+    var oldEditbarHeight = $('#editbar').height();
     var beforePrint = function() {
       var padId = pad.getPadId(); // get the pad ID
       var padHTMLUrl = padId + "/export/html"; // get the export URL
       $('#editbar').height(0); // it wont print if editbar doesn't exist..
       //  $('#editbar, #editorcontainerbox').hide(); // hide other contents
       if($('#print').length == 0){ // append if doesnt exist
-      $('body').append("<div id='print' style='position:fixed; top: 0; left: 0; right: 0; bottom: 0;z-index:9999'></div>")
+        $('body').append("<div id='print' style='position:fixed; margin-top:20px; top: 0; left: 0; right: 0; bottom: 0;z-index:9999'></div>")
       }
-      $.get(padHTMLUrl, function(data) {
-        $('#print').html(data);
-        //  alert('Load was performed.');
-        });
-        //    console.log('Functionality to run before printing.');
-      };
-      var afterPrint = function() {
-      //    console.log('Functionality to run after printing');
-      };
 
-       if (window.matchMedia) {
-        var mediaQueryList = window.matchMedia('print');
-        mediaQueryList.addListener(function(mql) {
-          if (mql.matches) {
-            beforePrint();
-          } else {
-            afterPrint();
-          }
-        });
-      }
-      window.onbeforeprint = beforePrint;
-      window.onafterprint = afterPrint;
+      $.get(padHTMLUrl, function(data) {
+        $('#print').show();
+        $('#print').html(data);
+      });
+    };
+
+    var afterPrint = function() {
+      setTimeout(function(){
+        $('#print').hide();
+        $('#editbar').height(oldEditbarHeight); 
+      }, 500);
+    };
+
+    if (window.matchMedia) {
+      var mediaQueryList = window.matchMedia('print');
+      mediaQueryList.addListener(function(mql) {
+        if (mql.matches) {
+          beforePrint();
+        } else {
+          afterPrint();
+        }
+      });
+    }
+    window.onbeforeprint = beforePrint;
+    window.onafterprint = afterPrint;
   }());
 }
 
