2643f708b8c439728363f1582178c5747ed1a2e6
John McLear <john@mclear.co.uk>
2013-11-21 15:19:19 +0000

    working cross browser printing with styles

--- static/js/print.js
+++ static/js/print.js
@@ -1,33 +1,15 @@
 exports.postAceInit = function(name, context, cb){
 
+  $('body').append("<div id='print'></div>");
+
   (function() { // From http://stackoverflow.com/questions/1234008/detecting-browser-print-event/11060206#11060206 -- License unknown
 
     var oldEditbarHeight = $('#editbar').height();
     var beforePrint = function() {
-      var padId = pad.getPadId(); // get the pad ID
-      var ts = new Date().getTime(); // Get the timestamp
-      var padHTMLUrl = padId + "/export/html?ts="+ts; // get the export URL
-      $('#chaticon').hide();
-      $('#editorcontainerbox').hide();
-      $('#editbar').height(0); // it wont print if editbar doesn't exist..
-      //  $('#editbar, #editorcontainerbox').hide(); // hide other contents
-      if($('#print').length == 0){ // append if doesnt exist
-        $('body').append("<div id='print' style='margin-top:40px; top: 0; left: 0; right: 0; height:100%; overflow:auto;z-index:9999; overflow-x:hidden;'></div>")
-      }
-
-      $.get(padHTMLUrl, function(data) {
-        $('#print').show();
-        $('#print').html(data);
-      });
+      $('#print').html($('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").html());
     };
 
     var afterPrint = function() {
-      setTimeout(function(){
-        $('#print').hide();
-        $('#chaticon').show();
-        $('#editorcontainerbox').show();
-        $('#editbar').height(oldEditbarHeight); 
-      }, 500);
     };
 
     if (window.matchMedia) {
