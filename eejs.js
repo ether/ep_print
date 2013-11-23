74e8c3bd458a101a9b8486d5fd8eb0d93909b0bf
John McLear <john@mclear.co.uk>
2013-11-23 20:23:38 +0000

    bump

--- eejs.js
+++ eejs.js
@@ -1,5 +1,4 @@
 exports.eejsBlock_styles = function (hook_name, args, cb) {
   args.content = args.content + "<link href='../static/plugins/ep_print/static/css/print.css' rel='stylesheet'>";
-  console.log("FOOO");
   return cb();
 }
